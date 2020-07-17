057633d081667eb17cf1a726bb3773deaab5d788
leandro <leandro.ecabello@gmail.com>
2020-07-17 20:50:36 -0300

    add deleteRoute and condition in post

--- src/app/routes/restaurants.routes.js
+++ src/app/routes/restaurants.routes.js
@@ -3,7 +3,7 @@ const router = Router()
 const fs = require('fs')
 
 const jsonRestaurants = fs.readFileSync('src/restaurants.json', 'utf-8')
-const restaurants = JSON.parse(jsonRestaurants) 
+let restaurants = JSON.parse(jsonRestaurants) 
 
 router
     .get('/', (req, res) => {
@@ -15,21 +15,36 @@ router
         
         const { name , kindOfRestaurant, specials = [] } = req.body
         
-        
         const newRestaurant = {
             name,
             kindOfRestaurant,
             specials      
         }
         
+        console.log(newRestaurant.name)
 
+        for( let r of restaurants) {
+            if(r.name === newRestaurant.name) {
+                console.log('nombre ya existe')
+                res.status(400).json({ message: 'The restaurant name already exists' })
+                return
+            }
+        }
+    
         restaurants.push(newRestaurant)
         const jsonRestaurants = JSON.stringify(restaurants)
         fs.writeFileSync('src/restaurants.json', jsonRestaurants, 'utf-8')
-                
-        res.status(201).json(newRestaurant)
+        res.status(201).json({ message : 'Restaurant added successfully'})
+        console.log('added successfully')
         
+        console.log(newRestaurant)
+    })
 
+    .delete('/delete/:name', (req, res) => {
+        restaurants = restaurants.filter( restaurant => restaurant.name != req.params.name)
+        const jsonRestaurants = JSON.stringify(restaurants)  
+        fs.writeFileSync('src/restaurants.json', jsonRestaurants, 'utf-8')
+        console.log('delete')    
     })
 
 module.exports = router    
